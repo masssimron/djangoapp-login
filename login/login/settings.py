75cc4bd076a265f80d4cdf8bc68f0d962b8c770b
masssimron <54320950+masssimron@users.noreply.github.com>
2021-07-08 22:15:50 +0530

    Update settings.py

--- login/login/settings.py
+++ login/login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+%m(k917(@^dn3das*ru_k#!mtba@q2p^iht*&^oo3l)m%a29-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
